4d5df5e79b9e10493e90370694f660fe349cb10c
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-05 20:08:27 +0300

    change .gitignore and tweaking models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Main feature of this application
+# Contains information about what is the user doing and when they started
 class Entry < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+# The User, managed by Devise
 class User < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :entries
 end
