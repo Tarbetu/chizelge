4545ee542bb09fe2670001d4047ef8d954bf70fb
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-05 18:10:58 +0300

    scaffolding entry model

---
+++ app/views/entries/_entry.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entry, :id, :comment, :type, :finished_at, :created_at, :updated_at
+json.url entry_url(entry, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root "home#index"
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  resources :entries
+  devise_for :users
   get 'home/index'
+
+  # if user_signed_in?
+  #   root "entry#index"
+  # else
+  root "home#index"
+  # end
 end
