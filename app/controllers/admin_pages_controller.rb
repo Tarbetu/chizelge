bc6d22d077b949d1630ceb857a00d3e6845b3b48
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-07 21:30:57 +0300

    adding searching around database

--- app/controllers/admin_pages_controller.rb
+++ app/controllers/admin_pages_controller.rb
@@ -3,14 +3,48 @@
 # Controller for Users with "Admin" role
 class AdminPagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_user
 
   # GET /admin_pages/dashboard
   def dashboard
-    redirect_to root_path unless User.find(current_user["id"]).admin?
-
     @list_everyting = pagy Entry.everything
   end
 
   # GET /admin_pages/result
-  def result; end
+  def result
+    email_addresses = params[:email].split " "
+    categories = params[:category].split " "
+    from_date = parsing_param_to_datetime(:from)
+    end_date = parsing_param_to_datetime(:end)
+    range = from_date..end_date
+    @pagy, @entries = pagy Entry.joins(:user).where(created_at: range).and(
+      if email_addresses.empty?
+        User.all
+      else
+        User.where(email: email_addresses)
+      end
+    ).and(
+      if categories.empty?
+        Entry.all
+      else
+        Entry.where(category: categories)
+      end
+    )
+  end
+
+  private
+
+  def parsing_param_to_datetime(date_type)
+    args = []
+    5.times do |num|
+      num += 1
+      args << params[:"#{date_type}(#{num}i)"].to_i
+    end
+
+    DateTime.new(*args)
+  end
+
+  def check_user
+    render file: "public/404.html", status: :unauthorized unless User.find(current_user["id"]).admin?
+  end
 end
