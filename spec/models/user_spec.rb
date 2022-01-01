b3d3447ccc23ce1ceb55db82ddc7ce3774f8dc47
Emrecan Şuşter <emrecansuster@tuta.io>
2022-01-01 22:28:50 +0300

    I don't understand why it raise 'Factory already registered' error

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # before(:all) { FactoryBot.create(:user) }
+  context "validations" do
+    it "is valid if email and password, " do
+
+    end
+  end
+
+  describe "#working?" do
+
+  end
+end
